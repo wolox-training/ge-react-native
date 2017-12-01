65d1c8effb8c731815c28bd07e17838a89bf2fc8
Gonzalo Escalante <gonzalo.escalante@wolox.com.ar>
2017-12-01 11:20:31 -0300

    added catalog list of books getting data from a local JSON + styles for the book catalog

--- wbooks/src/app/screens/dashboard/components/Catalog.js
+++ wbooks/src/app/screens/dashboard/components/Catalog.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
+import booksJson from '../../../../../resources/books.json'; 
+import Book from './Book';
 
 class Catalog extends Component {
 	constructor(props){
-		super(props);
-		this.state={filters:props.filters}
+		super(props);		
+		this.state={filters:props.filters, books: booksJson};	
 	}
+
+
 	render(){
+		const listBooks = this.state.books.map((book) => 
+			<Book image_url={book.image_url} alt={book.title} title={book.title} author={book.author}/>)
+
 		return (
-			<div>
-			{JSON.stringify(this.state.filters)}
+			<div className="catalog">
+				{listBooks}
 			</div>
 			);
 	}
