8d0c73ef09c8d9bd8d31781814a1e5a560e75a3f
Gonzalo Escalante <gonzalo.escalante@wolox.com.ar>
2017-12-04 16:05:55 -0300

    cleaned some code

--- wbooks/src/app/screens/dashboard/components/Search/index.js
+++ wbooks/src/app/screens/dashboard/components/Search/index.js
@@ -1,14 +1,10 @@
 import React, {Component} from 'react';
 import './style.css';
-<<<<<<< HEAD
 import * as constants from './constants';
-=======
->>>>>>> 6f65eed... added functionality to the catalog now displaying the books nicely, and also functionality to the search filters
 
 class Search extends Component {
   state = {updateFilters : this.props.updateFilters};
 
-<<<<<<< HEAD
   handleTextChange = (e) => {
     this.setState({titleFilter: e.target.value})
   }
@@ -18,24 +14,12 @@ class Search extends Component {
   }
 
   handleSubmit = (e) => {
-=======
-  handleTextChange(e){
-    this.setState({titleFilter: e.target.value})
-  }
-
-  handleSelectChange(e){
-    this.setState({dropdownFilter: e.target.value});
-  }
-
-  handleSubmit(e){
->>>>>>> 6f65eed... added functionality to the catalog now displaying the books nicely, and also functionality to the search filters
     e.preventDefault();
    this.props.updateFilters(this.state.dropdownFilter, this.state.titleFilter); 
   }
 
   render(){
     return (
-<<<<<<< HEAD
       <form onSubmit={this.handleSubmit } className="search">
         <select className="filter-dropdown" defaultValue="" onChange={this.handleSelectChange}>
           <option value="">Seleccionar filtro </option>
@@ -47,19 +31,6 @@ class Search extends Component {
         </select>
         <div className="filter-text">
           <input className="text-input" type="text" placeholder="Buscar..." onChange={this.handleTextChange}/>
-=======
-      <form onSubmit={this.handleSubmit.bind(this)} className="search">
-        <select className="filter-dropdown" defaultValue="" onChange={this.handleSelectChange.bind(this)}>
-          <option value="">Seleccionar filtro </option>
-          <option value="short story">Short story</option>
-          <option value="Suspense">Suspense</option>
-          <option value="Thriller">Thriller</option>
-          <option value="Fiction">Fiction</option>
-          <option value="Other">Other</option>
-        </select>
-        <div className="filter-text">
-          <input className="text-input" type="text" placeholder="Buscar..." onChange={this.handleTextChange.bind(this)}/>
->>>>>>> 6f65eed... added functionality to the catalog now displaying the books nicely, and also functionality to the search filters
           <input type="submit" value="" className="search-button"/>
         </div>
      </form>);
