d7829ca3e6411a75dbd457d7a13e59d97641f4b8
Gonzalo Escalante <gonzalo.escalante@wolox.com.ar>
2017-12-27 18:34:58 -0300

    added dependencies and structure to begin using redux

---
+++ WChat/src/app/redux/message/reducer.js
@@ -0,0 +1,18 @@
+import Immutable from 'seamless-immutable';
+import actionTypes from '../actionTypes';
+
+const initialState = {
+  contacts: [],
+};
+
+const message = (state = initialState, action) => {
+  switch (action.type){
+    case actionTypes.GET_CONTACTS:
+      return Immutable.merge(state, {
+        contacts: action.contacts
+      })
+    default:
+      return state;
+  }
+
+export default message;
