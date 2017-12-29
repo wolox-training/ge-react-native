26f3f6082cd1e02fd02cc98698b6309dffb9181e
Gonzalo Escalante <gonzalo.escalante@wolox.com.ar>
2017-12-29 18:10:14 -0300

    now showing contacts and groups from the api and showing last message on the contacts list

--- WChat/src/app/index.js
+++ WChat/src/app/index.js
@@ -7,6 +7,14 @@ import RootNavigator from './components/RootNavigator';
 import { connect } from 'react-redux';
 import userActions from './redux/user/actions';
 
+  render(){
+    return (
+      <View style={styles.container}>
+        <RootNavigator screenProps={{user: this.props.user, appLoading: this.props.appLoading}}/>
+      </View>
+    );
+  }
+}
 
 class App extends Component {
 
