967695cfd3dae39fba3d74cf14e7a469340f3146
Gonzalo Escalante <gonzalo.escalante@wolox.com.ar>
2018-01-02 18:28:11 -0300

    again forgot one small detail

--- WChat/src/app/components/FilterAddBar/index.js
+++ WChat/src/app/components/FilterAddBar/index.js
@@ -13,11 +13,12 @@ class FilterAddBar extends Component {
   state = {filter: '', filtering: false}
   handleCloseButtonPress = () => this.setState({filtering: false})
   handleSearchButtonPress = () => this.setState({filtering:true})
+  handleChangeText = (filter) => this.setState({filter})
   render(){
     return (
     <View style={styles.filterAddBarContainer}>
       <Ionicons style={this.state.filtering ? styles.hiddenNode : styles.filterSearchIcon} name="md-search" size={26} onPress={this.handleSearchButtonPress}/>
-      <TextInput style={this.state.filtering ? styles.filterTextInput : styles.hiddenNode} value={this.state.filter} onChangeText={(filter) => this.setState({filter})}/>
+      <TextInput style={this.state.filtering ? styles.filterTextInput : styles.hiddenNode} value={this.state.filter} onChangeText={this.handleChangeText}/>
       <Text style={this.state.filtering ? styles.filterClose : styles.hiddenNode} onPress={this.handleCloseButtonPress}>&#10006;</Text>
       <Ionicons style={this.state.filtering ? styles.hiddenNode : styles.filterAdd} name="md-add" size={26}/>
     </View>
