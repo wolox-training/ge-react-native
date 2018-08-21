1f798d786dff1adf1e2b8456bc69c61b04a930c9
Gonzalo Escalante <gonnie@MacBook-Pro-de-wolox-4.local>
2018-08-21 14:38:18 -0300

    i should use whatever name i want you prick

--- WChat/src/app/components/FilterAddBar/index.js
+++ WChat/src/app/components/FilterAddBar/index.js
@@ -5,7 +5,7 @@ import {
   StyleSheet,
   TextInput
 } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons'; 
+import IonIcons from 'react-native-vector-icons/Ionicons';
 import styles from './styles'
 
 
@@ -17,10 +17,10 @@ class FilterAddBar extends Component {
   render(){
     return (
     <View style={styles.filterAddBarContainer}>
-      <Ionicons style={this.state.filtering ? styles.hiddenNode : styles.filterSearchIcon} name="md-search" size={26} onPress={this.handleSearchButtonPress}/>
+      <IonIcons style={this.state.filtering ? styles.hiddenNode : styles.filterSearchIcon} name="md-search" size={26} onPress={this.handleSearchButtonPress}/>
       <TextInput style={this.state.filtering ? styles.filterTextInput : styles.hiddenNode} value={this.state.filter} onChangeText={this.handleChangeText}/>
       <Text style={this.state.filtering ? styles.filterClose : styles.hiddenNode} onPress={this.handleCloseButtonPress}>&#10006;</Text>
-      <Ionicons style={this.state.filtering ? styles.hiddenNode : styles.filterAdd} name="md-add" size={26} onPress={this.props.handleAdd}/>
+      <IonIcons style={this.state.filtering ? styles.hiddenNode : styles.filterAdd} name="md-add" size={26} onPress={this.props.handleAdd}/>
     </View>
     );
   }
